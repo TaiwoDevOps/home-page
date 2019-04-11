5d03909e7b61ab76441492779e42bda697fd414f
Adisa Taiwo <adisataiwojoshua@gmail.com>
2019-04-11 09:13:11 +0100

    Uvanest Capital Limited Website

--- js/main.js
+++ js/main.js
@@ -38,3 +38,19 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+// smooth scrolling
+$('.nav-links a').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+      const hash = this.hash;
+  
+      $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      )
+    }
+  });
